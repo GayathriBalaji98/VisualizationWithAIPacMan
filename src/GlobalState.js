bf0cbea4d8bfbd364f69507d498737500e9ced41
GayathriBalaji98 <113820232+GayathriBalaji98@users.noreply.github.com>
2024-11-17 16:09:27 -0600

    Add files via upload

---
+++ src/GlobalState.js
@@ -0,0 +1,22 @@
+import { atom } from "jotai";
+import { loadTruncatedMobileNet } from "./model";
+
+// ---- Configurations ----
+export const epochsAtom = atom(100); // Number of epochs
+export const batchSizeAtom = atom(1); // Selected batch size
+export const hiddenUnitsAtom = atom(100); // Number of hidden units
+export const learningRateAtom = atom(0.0001); // Learning rate
+export const gameRunningAtom = atom(false); // Game state
+export const predictionAtom = atom(null); // Current prediction
+
+// ---- Model Training ----
+export const modelAtom = atom(null); // Model
+export const truncatedMobileNetAtom = atom(loadTruncatedMobileNet()); // truncatedMobileNet
+export const imgSrcArrAtom = atom([]); // collected images, formate {src: string, label: string}
+
+// ---- UI Display ----
+export const lossAtom = atom(null); // Loss value
+export const trainingProgressAtom = atom(-1); // Training progress
+export const stopTrainingAtom = atom(false); // Flag to stop training
+
+
---
+++ src/components/DataCollection.js
@@ -0,0 +1,169 @@
+import Webcam from "react-webcam";
+import { Grid, Button, Box } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete"; 
+import {
+    ArrowUpward,
+    ArrowDownward,
+    ArrowBack,
+    ArrowForward,
+} from "@mui/icons-material/";
+import { useState, useRef } from "react";
+import { useAtom } from "jotai";
+import {
+    imgSrcArrAtom,
+    dataSetSizeAtom,
+    batchArrayAtom,
+    batchSizeAtom,
+    gameRunningAtom,
+} from "../GlobalState";
+
+const DIRECTIONS = {
+    up: <ArrowUpward />,
+    down: <ArrowDownward />,
+    left: <ArrowBack />,
+    right: <ArrowForward />,
+};
+
+export default function DataCollection({ webcamRef }) {
+    const [isCameraOn, setIsCameraOn] = useState(false);
+
+    // ---- Model Training ----
+    const [imgSrcArr, setImgSrcArr] = useAtom(imgSrcArrAtom);
+
+    // ---- Configurations ----
+    const [, setBatchSize] = useAtom(batchSizeAtom);
+    const [gameRunning] = useAtom(gameRunningAtom);
+
+    // ---- UI Display ----
+
+    const capture = (direction) => async () => {
+        // Capture image from webcam
+        const newImageSrc = webcamRef.current.getScreenshot();
+
+        // If image is not null, proceed with adding it to the dataset
+        if (newImageSrc) {
+
+            // Add example to the dataset
+            const newImageArr = [...imgSrcArr, { src: newImageSrc, label: direction }];
+            setImgSrcArr(newImageArr);
+            setBatchSize(Math.floor(newImageArr.length * 0.4));
+        }
+    };
+
+    const cameraPlaceholder = (
+        <Box
+            display="flex"
+            textAlign={"center"}
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+                p: 2,
+                border: "1px dashed grey",
+                height: "224px",
+                width: "224px",
+                margin: "auto",
+                backgroundColor: "#ddd",
+            }}
+        >
+            Camera is off
+        </Box>
+    );
+
+    return (
+        <Grid container>
+            {/* first row */}
+
+            <Grid
+                item
+                xs={12}
+                sx={{ marginBottom: 2 }}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                flexDirection="column"
+            >
+                <Box textAlign="center">
+                    <Button
+                        variant="contained"
+                        onClick={() => setIsCameraOn(!isCameraOn)}
+                        disabled={gameRunning}
+                    >
+                        {" "}
+                        {isCameraOn ? "Stop" : "Start"} Camera
+                    </Button>
+                </Box>
+                <Box sx={{ marginTop: 1 }}>
+                    {isCameraOn ? (
+                        <Webcam
+                            mirrored
+                            width={224}
+                            height={224}
+                            ref={webcamRef}
+                            screenshotFormat="image/jpeg"
+                            videoConstraints={{
+                                width: 224,
+                                height: 224,
+                                facingMode: "user",
+                            }}
+                        />
+                    ) : (
+                        cameraPlaceholder
+                    )}
+                </Box>
+            </Grid>
+
+            {Object.keys(DIRECTIONS).map((directionKey) => {
+                return (
+                    <OneDirection
+                        key={directionKey}
+                        directionKey={directionKey}
+                        disabled={!isCameraOn}
+                        directionIcon={DIRECTIONS[directionKey]}
+                        onCapture={capture(directionKey)}
+                        dirImgSrcArr={imgSrcArr.filter((d) => d.label == directionKey)}
+                    />
+                );
+            })}
+        </Grid>
+    );
+}
+
+const OneDirection = ({ directionKey, directionIcon, onCapture, dirImgSrcArr, disabled }) => {
+    const [, setImgSrcArr] = useAtom(imgSrcArrAtom);
+    const handleDelete = () => {
+        setImgSrcArr((prevArr) => prevArr.filter((img) => img.label !== directionKey));
+    };
+
+    return (
+        <Grid item xs={3}>
+            <Box textAlign="center">
+                <Button
+                    variant="outlined"
+                    endIcon={directionIcon}
+                    onClick={onCapture}
+                    disabled={disabled}
+                >
+                    Add to
+                </Button>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleDelete}
+                    disabled={dirImgSrcArr.length === 0}
+                    sx={{ marginLeft: 1 }}
+                >
+                    <DeleteIcon></DeleteIcon>
+                </Button>
+            </Box>
+            <Box textAlign="center" sx={{ width: "100%", height: "100px" }}>
+                {dirImgSrcArr.length > 0 && (
+                    <img
+                        height={"100%"}
+                        src={dirImgSrcArr[dirImgSrcArr.length - 1].src}
+                        style={{ padding: "2px" }}
+                    />
+                )}
+            </Box>
+        </Grid>
+    );
+};
